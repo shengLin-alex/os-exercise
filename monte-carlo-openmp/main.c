170f0871a951dec503f9cee6b0644be84d130537
shenglin-alex <a182820998@gmail.com>
2018-10-31 12:25:56 +0800

    4.18 finish

---
+++ monte-carlo-openmp/main.c
@@ -0,0 +1,31 @@
+#include "stdlib.h"
+#include "omp.h"
+#include "string.h"
+#include "math.h"
+#include "stdio.h"
+
+int main(int argc, char** argv) {
+    int amountOfPoints = 0;
+    int pointsInCircle = 0;
+
+    while (amountOfPoints <= 0) {
+        printf("Amount of points:\n");
+        scanf("%d", &amountOfPoints);
+    }
+
+#pragma omp parallel for num_threads(8)
+    for (int i = 0; i < amountOfPoints; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+
+        // x^2 + y^2 < 1 means points in circle
+        if (((x * x) + (y * y)) < 1) {
+            pointsInCircle++;
+        }
+    }
+
+    double pi = 4.0 * pointsInCircle / amountOfPoints;
+    printf("%d points; PI = %f", amountOfPoints, pi);
+
+    return 0;
+}
