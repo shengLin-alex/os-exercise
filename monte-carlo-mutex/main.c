080cc1dd614249ec0c17197cd13349fec76dc28c
shenglin-alex <a182820998@gmail.com>
2018-10-31 12:54:28 +0800

    6.33 finish

--- monte-carlo-mutex/main.c
+++ monte-carlo-mutex/main.c
@@ -4,34 +4,61 @@
 #include <time.h>
 #include <string.h>
 
-int amountOfPoints = 0;
+int pointsPerThread = 0;
 int pointsInCircle = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* run(void* x) {
-    for (int i = 0; i < amountOfPoints; i++) {
+    int pointsInCirclePerThread = 0;
+    unsigned int randState = rand();
+
+    for (int i = 0; i < pointsPerThread; i++) {
         double x = (double)rand() / RAND_MAX;
         double y = (double)rand() / RAND_MAX;
 
         // x^2 + y^2 < 1 means points in circle
         if (((x * x) + (y * y)) < 1) {
-            pointsInCircle++;
+            pointsInCirclePerThread++;
         }
     }
+
+    // lock and accumulate points in circle per thread to global pointsInCircle
+    pthread_mutex_lock(&mutex);
+    pointsInCircle += pointsInCirclePerThread;
+    pthread_mutex_unlock(&mutex);
 
     return NULL;
 }
 
 int main(int argc, char** argv) {
     srand(time(NULL));
-    pthread_t thread;
 
-    while (amountOfPoints <= 0) {
+    int threadCount = 0;
+    int amountOfPoints = 0;
+
+    while (amountOfPoints <= 0 && threadCount <= 0) {
         printf("Amount of points:\n");
         scanf("%d", &amountOfPoints);
+        printf("The number of thread:\n");
+        scanf("%d", &threadCount);
     }
 
-    pthread_create(&thread, NULL, &run, NULL);
-    pthread_join(thread, NULL);
+    pointsPerThread = amountOfPoints / threadCount;
+    
+    pthread_t* threads = malloc(threadCount * sizeof(pthread_t));
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_create(&threads[i], &attr, run, NULL);
+    }
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    free(threads);
 
     double pi = 4.0 * pointsInCircle / amountOfPoints;
     printf("%d points; PI = %f", amountOfPoints, pi);
